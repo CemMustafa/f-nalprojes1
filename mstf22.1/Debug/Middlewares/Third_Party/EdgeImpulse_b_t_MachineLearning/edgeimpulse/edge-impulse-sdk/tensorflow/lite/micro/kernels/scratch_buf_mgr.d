7f678134b08fdc591faf6aec55e9dc0ace66a202
CemMustafa <118385916+CemMustafa@users.noreply.github.com>
2023-01-18 15:59:18 +0300

    fınalproject21

---
+++ mstf22.1/Debug/Middlewares/Third_Party/EdgeImpulse_b_t_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buf_mgr.d
@@ -0,0 +1,6 @@
+Middlewares/Third_Party/EdgeImpulse_b_t_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buf_mgr.o: \
+ ../Middlewares/Third_Party/EdgeImpulse_b_t_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buf_mgr.cpp \
+ c:\users\mustafa\ cem\desktop\mstf22.1\middlewares\third_party\edgeimpulse_b_t_machinelearning\edgeimpulse\edge-impulse-sdk\classifier\ei_classifier_config.h \
+ ../Middlewares/Third_Party/EdgeImpulse_b_t_MachineLearning/edgeimpulse/tflite-model/trained_model_ops_define.h
+c:\users\mustafa\ cem\desktop\mstf22.1\middlewares\third_party\edgeimpulse_b_t_machinelearning\edgeimpulse\edge-impulse-sdk\classifier\ei_classifier_config.h:
+../Middlewares/Third_Party/EdgeImpulse_b_t_MachineLearning/edgeimpulse/tflite-model/trained_model_ops_define.h:
---
+++ mstf22.1/Debug/Middlewares/Third_Party/EdgeImpulse_b_t_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buffers.d
@@ -0,0 +1,6 @@
+Middlewares/Third_Party/EdgeImpulse_b_t_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buffers.o: \
+ ../Middlewares/Third_Party/EdgeImpulse_b_t_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buffers.cpp \
+ c:\users\mustafa\ cem\desktop\mstf22.1\middlewares\third_party\edgeimpulse_b_t_machinelearning\edgeimpulse\edge-impulse-sdk\classifier\ei_classifier_config.h \
+ ../Middlewares/Third_Party/EdgeImpulse_b_t_MachineLearning/edgeimpulse/tflite-model/trained_model_ops_define.h
+c:\users\mustafa\ cem\desktop\mstf22.1\middlewares\third_party\edgeimpulse_b_t_machinelearning\edgeimpulse\edge-impulse-sdk\classifier\ei_classifier_config.h:
+../Middlewares/Third_Party/EdgeImpulse_b_t_MachineLearning/edgeimpulse/tflite-model/trained_model_ops_define.h:
